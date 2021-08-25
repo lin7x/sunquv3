d78ac86b88608f4bb3001b5ad9ba2baf931b1e0e
Alexander <kevinsm49@gmail.com>
2021-08-25 18:32:01 -0500

    first commit

---
+++ nosotros.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" rel="preload" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="./images/sunqu-icon.svg">
+    <script src="./js/index.js"></script>
+    <script src="https://kit.fontawesome.com/c1eae85dfa.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include('./php/menu.php');
+    ?>
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,13 @@
+<nav class="menu">
+        <label class="logo"><img src="./images/Sunqu-logo.png" alt=""></label>
+        <ul class="menu_items">
+            <li><a href="http://35.188.153.76/index/">Inicio</a></li>
+            <li><a href="http://35.188.153.76/soluciones/">Soluciones</a></li>
+            <li class="active"><a href="http://35.188.153.76/nosotros/">Nosotros</a></li>
+            <li><a href="http://35.188.153.76/contacto/">Contacto</a></li>
+            <li><a href="http://35.188.153.76/blog/">Blog</a></li>
+        </ul>
+        <span class="btn_menu">
+            <i class="fa fa-bars"></i>
+        </span>
+    </nav>
